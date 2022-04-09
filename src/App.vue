d9c55c25ca5b8dd74691ed3940b6d75a42df0a7d
mehmet <>
2022-04-09 07:46:40 +0300

    Recursive table component created. Bugs fixed.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RecursiveTable @remove-record="removeRecord"
+                  :row="0" :level="0"
+                  :records="this.source"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import  { v4 as uuidv4 } from 'uuid'
+import RecursiveTable from './components/RecursiveTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RecursiveTable
+  },
+  data() {
+    return {
+      source: []
+    }
+  },
+  created() {
+    this.getSource().then((r) => {
+      this.setIdForEachRecord(r)
+      this.source = r
+    })
+  },
+  methods: {
+    setIdForEachRecord(records) {
+      records.forEach(record => {
+        record.data['custom_id'] = uuidv4()
+        console.log(record.data.custom_id)
+        Object.keys(record.kids).forEach(key => {
+          this.setIdForEachRecord(record.kids[key].records)
+        })
+      })
+    },
+    removeRecord(record) {
+      this.source = this.findRemove(this.source, record)
+    },
+    findRemove(records, target) {
+      let newRecords = []
+      if (records) {
+        for (let i = 0; i < records.length; i++) {
+          const record = records[i]
+          let newRecord = {data: {}, kids: {}}
+          if (target.data !== record.data) {
+            newRecord.data = record.data
+            const keys = Object.keys(record.kids)
+            if (keys.length > 0) {
+              let key = keys[0]
+              let rs = this.findRemove(record.kids[key].records, target)
+              if (rs.length > 0)
+                newRecord.kids[key] = {records: rs}
+            }
+            newRecords.push(newRecord)
+          }
+        }
+      }
+      return newRecords
+    },
+    getSource() {
+      return fetch("/example-data.json")
+          .then(res => {
+            return res.json()
+          })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/RecursiveTable.vue
@@ -0,0 +1,86 @@
+<template>
+  <table v-if="records && records.length > 0" class="table table-sm table-bordered">
+    <thead>
+    <tr class="table-light">
+      <th></th>
+      <th v-for="key in getDataKeys(records[0].data)" :key="key"> {{ key }}</th>
+      <th></th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <template v-for="(item, i) in this.records" :key="item.data.custom_id">
+      <tr>
+        <td>
+          <button v-if="hasKids(item.kids)"
+                  :id="'btn-' + item.data.custom_id"
+                  @click="toggleRow(item.data.custom_id)"
+                  class="btn btn-outline-success btn-sm" :key="item.data.custom_id">
+            Show
+          </button>
+        </td>
+        <td v-for="d in getData(item.data)" :key="d.custom_id">
+          {{ d }}
+        </td>
+        <td>
+          <button @click="this.$emit('remove-record', item)"
+                  class="btn btn-outline-danger btn-sm"> X
+          </button>
+        </td>
+      </tr>
+
+      <tr v-if="Object.keys(item.kids).length > 0" :key="item.data.custom_id"
+          :id="'accordion-' + item.data.custom_id" class="d-none">
+        <td :colspan="Object.keys(item.data).length + 1"
+            :style="'padding-left:' + (level + 1) * 2 + 'em' ">
+          <div>
+            <span v-if="item.kids[getKey(item.kids)].records.length > 0" class="float-start small">
+              {{ getKey(item.kids).toUpperCase() }}
+            </span>
+            <RecursiveTable @remove-record="this.removeRecord" :row="i" :level="level+1"
+                            :records="item.kids[getKey(item.kids)].records"></RecursiveTable>
+          </div>
+        </td>
+      </tr>
+    </template>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'RecursiveTable',
+  props: ['row', 'level', 'records'],
+  methods: {
+    getDataKeys(data) {
+      return Object.keys(data).filter(x => x !== 'custom_id')
+    },
+    getData(data) {
+      let newData = {}
+      Object.keys(data).filter(key => {
+        if (key !== 'custom_id')
+          newData[key] = data[key]
+      })
+      return newData
+    },
+    removeRecord(record) {
+      this.$emit('remove-record', record)
+    },
+    toggleRow(id) {
+      console.log(id)
+      const el = document.getElementById(`accordion-${id}`)
+      if (el) {
+        el.classList.toggle('d-none')
+        const btn = document.getElementById(`btn-${id}`)
+        btn.innerText = el.classList.contains('d-none') ? 'Show' : 'Hide'
+      }
+    },
+    getKey(kids) {
+      return Object.keys(kids)[0]
+    },
+    hasKids(obj) {
+      return Object.keys(obj).length > 0
+    },
+  }
+}
+</script>
